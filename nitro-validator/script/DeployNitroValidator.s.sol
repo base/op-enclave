be82addc1fc0e8d6f0004da0d3a56979fa62083e
Michael de Hoog <michael.dehoog@coinbase.com>
2024-10-22 21:57:59 -1000

    Switch to forked NitroProver

--- nitro-validator/script/DeployNitroValidator.s.sol
+++ nitro-validator/script/DeployNitroValidator.s.sol
@@ -3,24 +3,29 @@ pragma solidity 0.8.24;
 
 import {Script} from "forge-std/Script.sol";
 import {console2 as console} from "forge-std/console2.sol";
+import {CertManager} from "@marlinprotocol/CertManager.sol";
 import {NitroValidator} from "src/NitroValidator.sol";
 
 /// @notice will deploy the singleton NitroValidatorContract to a deterministic address
 contract DeployNitroValidator is Script {
     bytes32 constant SALT = bytes32(uint256(0x4E313752304F5)); // todo
 
-    function run() public returns (address addr_) {
+    function run() public {
         vm.startBroadcast();
 
-        addr_ = address(new NitroValidator{salt: SALT}());
+        CertManager manager = new CertManager{salt: SALT}();
+        NitroValidator validator = new NitroValidator{salt: SALT}(manager);
 
-        console.log("NitroValidator to be deployed at:", addr_);
+        console.log("CertManager deployed at:", address(manager));
+        console.log("NitroValidator deployed at:", address(validator));
 
         // Save the address to the deployment file
-        string memory deploymentJson = string.concat("{", '"address": "', vm.toString(addr_), '"}');
+        string memory deploymentJson = string.concat(
+            "{", '"certManager": "', vm.toString(address(manager)),
+            '", "nitroValidator": "', vm.toString(address(validator)), '"}');
 
         vm.writeFile(
-            string.concat(vm.projectRoot(), "/deployments/", vm.toString(block.chainid), "-nitro-validator-deploy.json"),
+            string.concat(vm.projectRoot(), "/deployments/", vm.toString(block.chainid), "-deploy.json"),
             deploymentJson
         );
 
