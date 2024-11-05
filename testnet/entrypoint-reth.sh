a8cbb298ae2605e697d5d6bd839ec15ea496dc07
Michael de Hoog <michael.dehoog@coinbase.com>
2024-11-04 20:55:10 -1000

    Fix entrypoint

--- testnet/entrypoint-reth.sh
+++ testnet/entrypoint-reth.sh
@@ -3,18 +3,16 @@
 RETH_DATA_DIR=${RETH_DATA_DIR:-/data}
 echo "$L2_ENGINE_JWT" > /tmp/engine.jwt
 
-exec ./reth \
+exec ./reth node \
   --datadir="$RETH_DATA_DIR" \
   --chain="$GENESIS_FILE_PATH" \
   --http \
   --http.corsdomain="*" \
-  --http.vhosts="*" \
   --http.addr=0.0.0.0 \
   --http.port=8545 \
-  --http.api=web3,debug,eth,net,engine \
+  --http.api=web3,debug,eth,net \
   --authrpc.addr=0.0.0.0 \
   --authrpc.port=8551 \
-  --authrpc.vhosts="*" \
   --authrpc.jwtsecret=/tmp/engine.jwt \
   --port=30303 \
-  --db.read-transaction-timeout=0
+  --rpc.eth-proof-window=10000
