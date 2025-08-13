4f3aa38a1b26d02b22ced6cbd2ea47ef3fdacf1d
Micke <155267459+reallesee@users.noreply.github.com>
2025-08-13 23:15:15 +0200

    docs: Remove outdated TODO comment in reth client (#87)
    * docs: Remove outdated TODO comment in reth client
    
    * Update reth_client.go

--- op-proposer/proposer/reth_client.go
+++ op-proposer/proposer/reth_client.go
@@ -21,18 +21,16 @@ func NewRethClient(client *ethclient.Client, metrics caching.Metrics) Client {
 }
 
 func (e *rethClient) ExecutionWitness(ctx context.Context, hash common.Hash) (*stateless.ExecutionWitness, error) {
-	header, err := e.HeaderByHash(ctx, hash)
-	if err != nil {
-		return nil, err
-	}
-
 	var witness stateless.ExecutionWitness
-	// TODO it would be nice if reth accepted a tx hash parameter here
-	if err := e.client.Client().CallContext(ctx, &witness, "debug_executionWitness", "0x"+header.Number.Text(16)); err != nil {
+	if err := e.client.Client().CallContext(ctx, &witness, "debug_executionWitness", hash.Hex()); err != nil {
 		return nil, err
 	}
 
 	// reth doesn't return required headers (for BLOCKHASH), so eagerly populate them all:
+	header, err := e.HeaderByHash(ctx, hash)
+	if err != nil {
+		return nil, err
+	}
 	parentHash := header.ParentHash
 	history := int(min(256, header.Number.Uint64()))
 	for i := 0; i < history; i++ {
